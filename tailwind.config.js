df4e50bad2c4a82e93140e242c25c4c2a80dd5dd
Twitch-defi <igegodstime0@gmail.com>
2024-11-26 16:19:19 +0100

    Loopstudio website

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    screens: {
+      sm:"340px",
+      md:"540px",
+      lg:"768px",
+      xl:"1180px"
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/images/image-hero.jpg')"
+      },
+    },
+  },
+  plugins: [],
+}
+
