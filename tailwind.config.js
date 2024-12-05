1f642ec05f605a18d98493dfd179a88e05872e5b
Twitch-defi <igegodstime0@gmail.com>
2024-12-05 17:58:53 +0100

    website update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    screens: {
-      sm:"340px",
-      md:"540px",
-      lg:"768px",
-      xl:"1180px"
-    },
+   
     extend: {
       backgroundImage: {
         'hero-pattern': "url('./images/image-hero.jpg')"
